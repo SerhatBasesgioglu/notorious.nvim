8f87010e5fb37ebea32d707a84c9985500dcd9c1
Serhat Başesgioğlu <55340640+SerhatBasesgioglu@users.noreply.github.com>
2025-10-05 15:16:53 +0300

    feat!: remove id field from metada

--- lua/notorious/create.lua
+++ lua/notorious/create.lua
@@ -5,14 +5,6 @@ local cfg = require("notorious.config")
 local notes_dir = cfg.notes_dir
 local extension = cfg.extension
 
-local function random_id()
-	local t = {}
-	for _ = 1, 4 do
-		table.insert(t, string.char(math.random(97, 122)))
-	end
-	return table.concat(t)
-end
-
 local function ensure_normal_window()
 	for _, win in ipairs(vim.api.nvim_list_wins()) do
 		local buf = vim.api.nvim_win_get_buf(win)
@@ -33,7 +25,6 @@ function M.new_note()
 
 	vim.fn.mkdir(notes_dir, "p")
 
-	local id = os.date("%Y%m%d") .. "-" .. random_id()
 	local filename = string.format("%s%s", title:gsub("%s+", "-"):lower(), extension)
 	local path = notes_dir .. "/" .. filename
 
@@ -44,7 +35,6 @@ function M.new_note()
 
 	local header = {
 		"---",
-		"id: " .. id,
 		"title: " .. title,
 		"tags: []",
 		"created " .. os.date("%Y-%m-%d %H:%M"),
